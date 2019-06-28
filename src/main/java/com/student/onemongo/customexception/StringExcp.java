de90a40605e66a21cf7060d85e41a33251182c91
Chandra Bhan Kumhar <chandrabhan.dz@gmail.com>
2019-06-28 10:14:51 +0530

    Alpha Version Spring Security Admin and User Privileges Separate API Endpoints
    Changes
    *Json ignored for Password and Role in Pojo .
    
    Admin Endpoint is working for /students/admin/getall and CREATE operation
    
    And Also Working for /students/admin/getbyid/{id}
     Update Delete operations
    
     Further: Student Portal Login functionality to be made.

---
+++ src/main/java/com/student/onemongo/customexception/StringExcp.java
@@ -0,0 +1,30 @@
+package com.student.onemongo.customexception;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestControllerAdvice
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Name should be atleast 4 letters")
+public class StringExcp extends ResponseEntityExceptionHandler {
+
+    //@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "For Age and Standard Fields Please Enter Integer Only")
+    //@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(StringIndexOutOfBoundsException.class)
+    public final ResponseEntity<ErrorDetails> handleStringIndexOutOfBoundsException(StringIndexOutOfBoundsException ex, WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
+
+                request.getDescription(false));
+        System.out.println(new Date() + "\t" + ex.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+
+    }
+}
